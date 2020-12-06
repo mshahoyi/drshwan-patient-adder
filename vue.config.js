cc38ef2564211983e813f755290feaaf89517dea
Mohammed Shahoe <muyabb@yahoo.com>
2020-12-06 15:17:50 +0300

    changed public path"

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
 	transpileDependencies: ["vuetify"],
-	publicPath: process.env.NODE_ENV === "production" ? "/drshwan-patient-adder/" : "/",
+	publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
 };
